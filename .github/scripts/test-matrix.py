1d5da7a2a1303b23f730fe70c97e72f60796148a
Eric Swanson <eric.swanson@dfinity.org>
2025-05-13 13:20:24 -0700

    chore: read directory to determine tests

---
+++ .github/scripts/test-matrix.py
@@ -0,0 +1,33 @@
+import os
+import json
+
+TEST_DIR = "bin/icp-cli/tests"
+
+MACOS_TESTS = ["hello_tests"]
+
+def test_names():
+    all_files = os.listdir(TEST_DIR)
+    rust_files = filter(lambda f: f.endswith(".rs"), all_files)
+    return [f"{filename[:-3]}" for filename in rust_files]
+
+include = []
+for test in test_names():
+    # Ubuntu: run everything
+    include.append({
+        "test": test,
+        "os": "ubuntu-22.04"
+    })
+
+    # macOS: only run selected tests
+    if test in MACOS_TESTS:
+        include.append({
+            "test": test,
+            "os": "macos-13"
+        })
+
+
+matrix = {
+    "include": include,
+}
+
+print(json.dumps(matrix))

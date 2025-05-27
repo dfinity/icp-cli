aa6c8ed648ec8419cc50dad76553084f5ede3522
Eric Swanson <eric.swanson@dfinity.org>
2025-05-27 15:10:27 -0700

    refione

--- bin/icp-cli/src/main.rs
+++ bin/icp-cli/src/main.rs
@@ -5,11 +5,9 @@ mod commands;
 mod project;
 
 #[tokio::main]
-async fn main() -> Result<(), DispatchError> {
+async fn main() {
     if let Err(e) = commands::dispatch(Cli::parse()).await {
         eprintln!("Error: {}", e);
         std::process::exit(1);
-    } else {
-        Ok(())
     }
 }

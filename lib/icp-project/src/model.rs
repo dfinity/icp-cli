c5b406d110f7228710065c7e1258d007428bd587
Or Ricon <rikonor@gmail.com>
2025-06-16 13:35:42 -0400

    put unit-tests under appropriate crate

--- lib/icp-project/src/model.rs
+++ lib/icp-project/src/model.rs
@@ -222,3 +222,286 @@ pub enum LoadProjectManifestError {
         path: Utf8PathBuf,
     },
 }
+
+#[cfg(test)]
+mod tests {
+    use camino_tempfile::tempdir;
+    use icp_adapter::script::{CommandField, ScriptAdapter};
+    use icp_canister::model::{Adapter, Build, CanisterManifest};
+
+    use crate::{
+        model::{LoadProjectManifestError, ProjectManifest},
+        structure::ProjectDirectoryStructure,
+    };
+
+    #[test]
+    fn empty_project() {
+        // Setup
+        let project_dir = tempdir().expect("failed to create temporary project directory");
+
+        // Write project-manifest
+        std::fs::write(
+            project_dir.path().join("icp.yaml"), // path
+            "",                                  // contents
+        )
+        .expect("failed to write project manifest");
+
+        // Load Project
+        let pds = ProjectDirectoryStructure::new(project_dir.path());
+        let pm = ProjectManifest::load(&pds).expect("failed to load project manifest");
+
+        // Verify no canisters were found
+        assert!(pm.canisters.is_empty());
+    }
+
+    #[test]
+    fn single_canister_project() {
+        // Setup
+        let project_dir = tempdir().expect("failed to create temporary project directory");
+
+        // Write project-manifest
+        let pm = r#"
+    canister:
+      name: canister-1
+      build:
+        adapter:
+          type: script
+          command: echo test
+    "#;
+
+        std::fs::write(
+            project_dir.path().join("icp.yaml"), // path
+            pm,                                  // contents
+        )
+        .expect("failed to write project manifest");
+
+        // Load Project
+        let pds = ProjectDirectoryStructure::new(project_dir.path());
+        let pm = ProjectManifest::load(&pds).expect("failed to load project manifest");
+
+        // Verify canister was loaded
+        let canisters = vec![(
+            project_dir.path().to_owned(),
+            CanisterManifest {
+                name: "canister-1".into(),
+                build: Build {
+                    adapter: Adapter::Script(ScriptAdapter {
+                        command: CommandField::Command("echo test".into()),
+                    }),
+                },
+            },
+        )];
+
+        assert_eq!(pm.canisters, canisters);
+    }
+
+    #[test]
+    fn multi_canister_project() {
+        // Setup
+        let project_dir = tempdir().expect("failed to create temporary project directory");
+
+        // Create canister directory
+        std::fs::create_dir(project_dir.path().join("canister-1"))
+            .expect("failed to create canister directory");
+
+        // Write canister-manifest
+        let cm = r#"
+    name: canister-1
+    build:
+      adapter:
+        type: script
+        command: echo test
+    "#;
+
+        std::fs::write(
+            project_dir.path().join("canister-1/canister.yaml"), // path
+            cm,                                                  // contents
+        )
+        .expect("failed to write canister manifest");
+
+        // Write project-manifest
+        let pm = r#"
+    canisters:
+      - canister-1
+    "#;
+
+        std::fs::write(
+            project_dir.path().join("icp.yaml"), // path
+            pm,                                  // contents
+        )
+        .expect("failed to write project manifest");
+
+        // Load Project
+        let pds = ProjectDirectoryStructure::new(project_dir.path());
+        let pm = ProjectManifest::load(&pds).expect("failed to load project manifest");
+
+        // Verify canister was loaded
+        let canisters = vec![(
+            project_dir.path().join("canister-1"),
+            CanisterManifest {
+                name: "canister-1".into(),
+                build: Build {
+                    adapter: Adapter::Script(ScriptAdapter {
+                        command: CommandField::Command("echo test".into()),
+                    }),
+                },
+            },
+        )];
+
+        assert_eq!(pm.canisters, canisters);
+    }
+
+    #[test]
+    fn invalid_project_manifest() {
+        // Setup
+        let project_dir = tempdir().expect("failed to create temporary project directory");
+
+        // Write project-manifest
+        std::fs::write(
+            project_dir.path().join("icp.yaml"), // path
+            "invalid-content",                   // contents
+        )
+        .expect("failed to write project manifest");
+
+        // Load Project
+        let pds = ProjectDirectoryStructure::new(project_dir.path());
+        let pm = ProjectManifest::load(&pds);
+
+        // Assert failure
+        assert!(matches!(pm, Err(LoadProjectManifestError::Parse { .. })));
+    }
+
+    #[test]
+    fn invalid_canister_manifest() {
+        // Setup
+        let project_dir = tempdir().expect("failed to create temporary project directory");
+
+        // Create canister directory
+        std::fs::create_dir(project_dir.path().join("canister-1"))
+            .expect("failed to create canister directory");
+
+        // Write canister-manifest
+        std::fs::write(
+            project_dir.path().join("canister-1/canister.yaml"), // path
+            "invalid-content",                                   // contents
+        )
+        .expect("failed to write canister manifest");
+
+        // Write project-manifest
+        let pm = r#"
+    canisters:
+      - canister-1
+    "#;
+
+        std::fs::write(
+            project_dir.path().join("icp.yaml"), // path
+            pm,                                  // contents
+        )
+        .expect("failed to write project manifest");
+
+        // Load Project
+        let pds = ProjectDirectoryStructure::new(project_dir.path());
+        let pm = ProjectManifest::load(&pds);
+
+        // Assert failure
+        assert!(matches!(
+            pm,
+            Err(LoadProjectManifestError::CanisterLoad { .. })
+        ));
+    }
+
+    #[test]
+    fn glob_path_non_canister() {
+        // Setup
+        let project_dir = tempdir().expect("failed to create temporary project directory");
+
+        // Create canister directory
+        std::fs::create_dir_all(project_dir.path().join("canisters/canister-1"))
+            .expect("failed to create canister directory");
+
+        // Skip writing canister-manifest
+        //
+
+        // Write project-manifest
+        let pm = r#"
+    canisters:
+      - canisters/*
+    "#;
+
+        std::fs::write(
+            project_dir.path().join("icp.yaml"), // path
+            pm,                                  // contents
+        )
+        .expect("failed to write project manifest");
+
+        // Load Project
+        let pds = ProjectDirectoryStructure::new(project_dir.path());
+        let pm = ProjectManifest::load(&pds).expect("failed to load project manifest");
+
+        // Verify no canisters were found
+        assert!(pm.canisters.is_empty());
+    }
+
+    #[test]
+    fn explicit_path_non_canister() {
+        // Setup
+        let project_dir = tempdir().expect("failed to create temporary project directory");
+
+        // Create canister directory
+        std::fs::create_dir(project_dir.path().join("canister-1"))
+            .expect("failed to create canister directory");
+
+        // Skip writing canister-manifest
+        //
+
+        // Write project-manifest
+        let pm = r#"
+    canisters:
+      - canister-1
+    "#;
+
+        std::fs::write(
+            project_dir.path().join("icp.yaml"), // path
+            pm,                                  // contents
+        )
+        .expect("failed to write project manifest");
+
+        // Load Project
+        let pds = ProjectDirectoryStructure::new(project_dir.path());
+        let pm = ProjectManifest::load(&pds);
+
+        // Assert failure
+        assert!(matches!(
+            pm,
+            Err(LoadProjectManifestError::NoManifest { .. })
+        ));
+    }
+
+    #[test]
+    fn invalid_glob_pattern() {
+        // Setup
+        let project_dir = tempdir().expect("failed to create temporary project directory");
+
+        // Write project-manifest
+        let pm = r#"
+    canisters:
+      - canisters/***
+    "#;
+
+        std::fs::write(
+            project_dir.path().join("icp.yaml"), // path
+            pm,                                  // contents
+        )
+        .expect("failed to write project manifest");
+
+        // Load Project
+        let pds = ProjectDirectoryStructure::new(project_dir.path());
+        let pm = ProjectManifest::load(&pds);
+
+        // Assert failure
+        assert!(matches!(
+            pm,
+            Err(LoadProjectManifestError::GlobPattern { .. })
+        ));
+    }
+}

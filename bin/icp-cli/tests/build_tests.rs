155e9dfeaec3d04a1fbac2ae7236ac1c327cef98
Or Ricon <rikonor@gmail.com>
2025-06-27 11:35:01 -0400

    add tests for multi-adapter build

--- bin/icp-cli/tests/build_tests.rs
+++ bin/icp-cli/tests/build_tests.rs
@@ -5,7 +5,7 @@ use icp_fs::fs::write;
 mod common;
 
 #[test]
-fn build_adapter_script() {
+fn build_adapter_script_single() {
     let env = TestEnv::new();
 
     // Setup project
@@ -23,6 +23,49 @@ fn build_adapter_script() {
             adapter:
               type: script
               command: sh -c 'cp {} $ICP_WASM_OUTPUT_PATH'
+        "#,
+        f.path()
+    );
+
+    write(
+        project_dir.join("icp.yaml"), // path
+        pm,                           // contents
+    )
+    .expect("failed to write project manifest");
+
+    // Invoke build
+    env.icp()
+        .current_dir(project_dir)
+        .args(["build"])
+        .assert()
+        .success();
+}
+
+#[test]
+fn build_adapter_script_multiple() {
+    let env = TestEnv::new();
+
+    // Setup project
+    let project_dir = env.create_project_dir("icp");
+
+    // Create temporary file
+    let f = NamedUtf8TempFile::new().expect("failed to create temporary file");
+
+    // Project manifest
+    let pm = format!(
+        r#"
+        canister:
+          name: my-canister
+          build:
+            - adapter:
+                type: script
+                command: echo "before"
+            - adapter:
+                type: script
+                command: sh -c 'cp {} $ICP_WASM_OUTPUT_PATH'
+            - adapter:
+                type: script
+                command: echo "after"
         "#,
         f.path()
     );

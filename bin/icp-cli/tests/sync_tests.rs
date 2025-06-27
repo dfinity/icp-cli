dd29de53cac00a677f67b85b2eeed55d6f48d83b
Or Ricon <rikonor@gmail.com>
2025-06-27 13:31:35 -0400

    add sync e2e tests

---
+++ bin/icp-cli/tests/sync_tests.rs
@@ -0,0 +1,93 @@
+use crate::common::TestEnv;
+use camino_tempfile::NamedUtf8TempFile;
+use icp_fs::fs::write;
+use predicates::{ord::eq, str::PredicateStrExt};
+
+mod common;
+
+#[test]
+fn sync_adapter_script_single() {
+    let env = TestEnv::new();
+
+    // Setup project
+    let project_dir = env.create_project_dir("icp");
+
+    // Create temporary file
+    let f = NamedUtf8TempFile::new().expect("failed to create temporary file");
+
+    // Project manifest
+    let pm = format!(
+        r#"
+        canister:
+          name: my-canister
+          build:
+            adapter:
+              type: script
+              command: sh -c 'cp {} $ICP_WASM_OUTPUT_PATH'
+          sync:
+            adapter:
+              type: script
+              command: echo "syncing"
+        "#,
+        f.path()
+    );
+
+    write(
+        project_dir.join("icp.yaml"), // path
+        pm,                           // contents
+    )
+    .expect("failed to write project manifest");
+
+    // Invoke sync
+    env.icp()
+        .current_dir(project_dir)
+        .args(["sync"])
+        .assert()
+        .success()
+        .stdout(eq("syncing").trim());
+}
+
+#[test]
+fn sync_adapter_script_multiple() {
+    let env = TestEnv::new();
+
+    // Setup project
+    let project_dir = env.create_project_dir("icp");
+
+    // Create temporary file
+    let f = NamedUtf8TempFile::new().expect("failed to create temporary file");
+
+    // Project manifest
+    let pm = format!(
+        r#"
+        canister:
+          name: my-canister
+          build:
+            adapter:
+              type: script
+              command: sh -c 'cp {} $ICP_WASM_OUTPUT_PATH'
+          sync:
+            - adapter:
+                type: script
+                command: echo "first"
+            - adapter:
+                type: script
+                command: echo "second"
+        "#,
+        f.path()
+    );
+
+    write(
+        project_dir.join("icp.yaml"), // path
+        pm,                           // contents
+    )
+    .expect("failed to write project manifest");
+
+    // Invoke build
+    env.icp()
+        .current_dir(project_dir)
+        .args(["sync"])
+        .assert()
+        .success()
+        .stdout(eq("first\nsecond").trim());
+}

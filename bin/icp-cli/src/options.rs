c2afc745e266574bf7b033fcf13b0615ef5cedcc
Eric Swanson <eric.swanson@dfinity.org>
2025-07-08 06:14:21 -0700

    refactor: move NetworkOpt to options/network.rs

--- bin/icp-cli/src/options.rs
+++ bin/icp-cli/src/options.rs
@@ -1,25 +1,3 @@
-use clap::{ArgGroup, Args};
+mod network;
 
-#[derive(Args, Clone, Debug, Default)]
-#[clap(
-    group(ArgGroup::new("network-select").multiple(false)),
-)]
-pub struct NetworkOpt {
-    /// Override the compute network to connect to. By default, the local network is used.
-    #[arg(long, env = "ICP_NETWORK", global(true), group = "network-select")]
-    network: Option<String>,
-
-    /// Shorthand for --network=ic.
-    #[clap(long, global(true), group = "network-select")]
-    ic: bool,
-}
-
-impl NetworkOpt {
-    pub fn name(&self) -> &str {
-        if self.ic {
-            "ic"
-        } else {
-            self.network.as_deref().unwrap_or("local")
-        }
-    }
-}
+pub use network::NetworkOpt;

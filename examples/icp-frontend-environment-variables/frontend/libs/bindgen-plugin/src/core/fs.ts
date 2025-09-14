fe2bcd27fd42e56da2d2357abc00b1df8f0412df
ilbertt <luca.bertelli@dfinity.org>
2025-09-14 15:58:42 +0200

    refactor: remove plugin source code

--- examples/icp-frontend-environment-variables/frontend/libs/bindgen-plugin/src/core/fs.ts
+++
@@ -1,9 +0,0 @@
-import { mkdir, rm } from "node:fs/promises";
-
-export async function ensureDir(dir: string) {
-  await mkdir(dir, { recursive: true });
-}
-
-export async function emptyDir(dir: string) {
-  await rm(dir, { recursive: true, force: true });
-}

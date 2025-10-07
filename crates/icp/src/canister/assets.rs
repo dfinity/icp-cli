12e6de81a17e8fb4f70f3b83c5c8214f086772cc
Or Ricon <rikonor@gmail.com>
2025-10-07 15:06:32 -0400

    update assets syncer

--- crates/icp/src/canister/assets.rs
+++ crates/icp/src/canister/assets.rs
@@ -1,3 +1,4 @@
+use anyhow::Context;
 use async_trait::async_trait;
 use ic_agent::Agent;
 use tokio::sync::mpsc::Sender;
@@ -13,85 +14,50 @@ impl Synchronize for Assets {
         step: &Step,
         params: &Params,
         agent: &Agent,
-        stdio: Option<Sender<String>>,
+        _: Option<Sender<String>>,
     ) -> Result<(), SynchronizeError> {
+        // Adapter
+        let adapter = match step {
+            Step::Assets(v) => v,
+            _ => panic!("expected assets adapter"),
+        };
+
+        // Prepare canister client
+        let canister = ic_utils::Canister::builder()
+            .with_canister_id(params.cid)
+            .with_agent(agent)
+            .build()
+            .context("failed to build canister client")?;
+
+        // Normalize `dir` field based on whether it's a single dir or multiple.
+        let dirs = adapter.dir.as_vec();
+
+        #[allow(clippy::disallowed_types)]
+        let dirs = dirs
+            .iter()
+            // Paths are specified relative to the canister path
+            .map(|p| params.path.join(p))
+            // Convert to PathBuf
+            .map(std::path::PathBuf::from)
+            .collect::<Vec<std::path::PathBuf>>();
+
+        #[allow(clippy::disallowed_types)]
+        let dirs: Vec<&std::path::Path> = dirs.iter().map(|p| p.as_path()).collect();
+
+        // ic-asset requires a logger, so provide it a nop logger
+        let logger = slog::Logger::root(slog::Discard, slog::o!());
+
+        // Synchronize assets to canister
+        ic_asset::sync(
+            &canister, // canister
+            &dirs,     // dirs
+            false,     // no_delete
+            &logger,   // logger
+            None,      // progress
+        )
+        .await
+        .context("failed to synchronize assets canister")?;
+
         Ok(())
     }
 }
-
-// /// Configuration for a custom canister build adapter.
-// #[derive(Clone, Debug, Deserialize, PartialEq, JsonSchema)]
-// pub struct AssetsAdapter {
-//     /// Directory used to synchronize an assets canister
-//     #[serde(flatten)]
-//     pub dir: DirField,
-// }
-
-// impl fmt::Display for AssetsAdapter {
-//     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-//         let dir = match &self.dir {
-//             DirField::Dir(d) => format!("directory: {d}"),
-//             DirField::Dirs(ds) => format!("{} directories", ds.len()),
-//         };
-
-//         write!(f, "({dir})")
-//     }
-// }
-
-// #[async_trait]
-// impl sync::Adapter for AssetsAdapter {
-//     async fn sync(
-//         &self,
-//         canister_path: &Path,
-//         canister_id: &Principal,
-//         agent: &Agent,
-//     ) -> Result<(), AdapterSyncError> {
-//         // Normalize `dir` field based on whether it's a single dir or multiple.
-//         let dirs = self.dir.as_vec();
-
-//         #[allow(clippy::disallowed_types)]
-//         let dirs = dirs
-//             .iter()
-//             // Paths are specified relative to the canister path
-//             .map(|p| canister_path.join(p))
-//             // Convert to PathBuf
-//             .map(std::path::PathBuf::from)
-//             .collect::<Vec<std::path::PathBuf>>();
-
-//         #[allow(clippy::disallowed_types)]
-//         let dirs: Vec<&std::path::Path> = dirs.iter().map(|p| p.as_path()).collect();
-
-//         // ic-asset requires a logger, so provide it a nop logger
-//         let logger = slog::Logger::root(slog::Discard, slog::o!());
-
-//         // Prepare canister client
-//         let canister = Canister::builder()
-//             .with_canister_id(canister_id.to_owned())
-//             .with_agent(agent)
-//             .build()
-//             .map_err(|err| AssetsAdapterSyncError::CanisterBuilder { source: err })?;
-
-//         // Synchronize assets to canister
-//         ic_asset::sync(
-//             &canister, // canister
-//             &dirs,     // dirs
-//             false,     // no_delete
-//             &logger,   // logger
-//             None,      // progress
-//         )
-//         .await
-//         .map_err(|err| AssetsAdapterSyncError::Sync { source: err })?;
-
-//         Ok(())
-//     }
-// }
-
-// #[derive(Debug, Snafu)]
-// #[snafu(context(suffix(SyncSnafu)))]
-// pub enum AssetsAdapterSyncError {
-//     #[snafu(transparent)]
-//     CanisterBuilder { source: CanisterBuilderError },
-
-//     #[snafu(transparent)]
-//     Sync { source: SyncError },
-// }

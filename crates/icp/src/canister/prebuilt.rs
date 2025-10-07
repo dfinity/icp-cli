877a9f74554e6a91e82d22fde5d57e6f36d4969d
Or Ricon <rikonor@gmail.com>
2025-10-07 15:13:30 -0400

    update prebuilt builder

--- crates/icp/src/canister/prebuilt.rs
+++ crates/icp/src/canister/prebuilt.rs
@@ -1,7 +1,17 @@
-use async_trait::async_trait;
-use tokio::sync::mpsc::Sender;
+use std::str::FromStr;
 
-use crate::canister::build::{Build, BuildError, Params, Step};
+use anyhow::{Context, anyhow};
+use async_trait::async_trait;
+use reqwest::{Client, Method, Request};
+use sha2::{Digest, Sha256};
+use tokio::sync::mpsc::Sender;
+use url::Url;
+
+use crate::{
+    canister::build::{Build, BuildError, Params, Step},
+    fs::{read, write},
+    manifest::adapter::prebuilt::SourceField,
+};
 
 pub struct Prebuilt;
 
@@ -11,117 +21,79 @@ impl Build for Prebuilt {
         &self,
         step: &Step,
         params: &Params,
-        stdio: Option<Sender<String>>,
+        _: Option<Sender<String>>,
     ) -> Result<(), BuildError> {
+        // Adapter
+        let adapter = match step {
+            Step::Prebuilt(v) => v,
+            _ => panic!("expected prebuilt adapter"),
+        };
+
+        let wasm = match &adapter.source {
+            // Local path
+            SourceField::Local(s) => {
+                read(&params.path.join(&s.path)).context("failed to read prebuilt canister file")?
+            }
+
+            // Remote url
+            SourceField::Remote(s) => {
+                // Initialize a new http client
+                let http_client = Client::new();
+
+                // Parse Url
+                let u = Url::from_str(&s.url).context("failed to parse prebuilt canister url")?;
+
+                // Construct request
+                let req = Request::new(
+                    Method::GET,  // method
+                    u.to_owned(), // url
+                );
+
+                // Execute request
+                let resp = http_client
+                    .execute(req)
+                    .await
+                    .context("failed to fetch prebuilt canister file")?;
+
+                let status = resp.status();
+
+                // Check for success
+                if !status.is_success() {
+                    return Err(anyhow!("http request failed {status}").into());
+                }
+
+                // Read response body
+                resp.bytes()
+                    .await
+                    .context("failed to read http response")?
+                    .to_vec()
+            }
+        };
+
+        // Verify the checksum if it's provided
+        if let Some(expected) = &adapter.sha256 {
+            // Calculate checksum
+            let actual = hex::encode({
+                let mut h = Sha256::new();
+                h.update(&wasm);
+                h.finalize()
+            });
+
+            // Verify Checksum
+            if &actual != expected {
+                return Err(
+                    anyhow!("checksum mismatch, expected: {expected}, actual: {actual}").into(),
+                );
+            }
+        }
+
+        // Set WASM file
+        write(
+            &params.output, // path
+            &wasm,          // contents
+        )
+        .context("failed to write wasm output")?;
+
         Ok(())
     }
 }
-
-// #[async_trait]
-// impl Adapter for PrebuiltAdapter {
-//     async fn compile(
-//         &self,
-//         canister_path: &Path,
-//         wasm_output_path: &Path,
-//     ) -> Result<(), AdapterCompileError> {
-//         let wasm = match &self.source {
-//             // Local path
-//             SourceField::Local(s) => read(&canister_path.join(&s.path))
-//                 .map_err(|err| PrebuiltAdapterCompileError::ReadFile { source: err })?,
-
-//             // Remote url
-//             SourceField::Remote(s) => {
-//                 // Initialize a new http client
-//                 let http_client = Client::new();
-
-//                 // Parse Url
-//                 let u = Url::from_str(&s.url)
-//                     .map_err(|err| PrebuiltAdapterCompileError::Url { source: err })?;
-
-//                 // Construct request
-//                 let req = Request::new(
-//                     Method::GET,  // method
-//                     u.to_owned(), // url
-//                 );
-
-//                 // Execute request
-//                 let resp = http_client
-//                     .execute(req)
-//                     .await
-//                     .map_err(|err| PrebuiltAdapterCompileError::Request { source: err })?;
-
-//                 let status = resp.status();
-
-//                 // Check for success
-//                 if !status.is_success() {
-//                     return Err(PrebuiltAdapterCompileError::Status {
-//                         url: u,
-//                         code: status,
-//                     }
-//                     .into());
-//                 }
-
-//                 // Read response body
-//                 resp.bytes()
-//                     .await
-//                     .map_err(|err| PrebuiltAdapterCompileError::Request { source: err })?
-//                     .to_vec()
-//             }
-//         };
-
-//         // Verify the checksum if it's provided
-//         if let Some(expected) = &self.sha256 {
-//             // Calculate checksum
-//             let actual = hex::encode({
-//                 let mut h = Sha256::new();
-//                 h.update(&wasm);
-//                 h.finalize()
-//             });
-
-//             // Verify Checksum
-//             if &actual != expected {
-//                 return Err(PrebuiltAdapterCompileError::Checksum {
-//                     expected: expected.to_owned(),
-//                     actual: actual.to_owned(),
-//                 }
-//                 .into());
-//             }
-//         }
-
-//         // Set WASM file
-//         write(
-//             wasm_output_path, // path
-//             &wasm,            // contents
-//         )
-//         .map_err(|err| PrebuiltAdapterCompileError::WriteFile { source: err })?;
-
-//         Ok(())
-//     }
-// }
-
-// #[derive(Debug, Snafu)]
-// pub enum PrebuiltAdapterCompileError {
-//     #[snafu(display("failed to read file"))]
-//     ReadFile { source: icp::fs::Error },
-
-//     #[snafu(transparent)]
-//     Url { source: url::ParseError },
-
-//     #[snafu(transparent)]
-//     Request { source: reqwest::Error },
-
-//     #[snafu(display("fetching {url} resulted in status-code: {code}"))]
-//     Status { url: Url, code: StatusCode },
-
-//     #[snafu(display(
-//         r#"
-//         resource has unexpected checksum.
-//             expected: {expected}
-//             actual: {actual}
-//         "#
-//     ))]
-//     Checksum { expected: String, actual: String },
-
-//     #[snafu(display("failed to write file"))]
-//     WriteFile { source: icp::fs::Error },
-// }

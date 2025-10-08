e1e51cbb4dc92ec325b3a3e672d1f5b8eb0ff115
Vivienne <vivienne.siffert@dfinity.org>
2025-10-08 10:16:25 +0200

    add expected behavior test

--- crates/icp-cli/tests/build_tests.rs
+++ crates/icp-cli/tests/build_tests.rs
@@ -1,6 +1,7 @@
 use crate::common::TestContext;
 use camino_tempfile::NamedUtf8TempFile as NamedTempFile;
 use icp::fs::write_string;
+use predicates::{prelude::PredicateBooleanExt, str::contains};
 
 mod common;
 
@@ -80,4 +81,49 @@ fn build_adapter_script_multiple() {
         .args(["build"])
         .assert()
         .success();
+}
+
+#[test]
+fn build_adapter_display_failing_build_output() {
+    let ctx = TestContext::new();
+
+    // Setup project
+    let project_dir = ctx.create_project_dir("icp");
+
+    // Project manifest
+    let pm = r#"
+        canister:
+          name: my-canister
+          build:
+            steps:
+              - type: script
+                command: echo "success 1"
+              - type: script
+                command: echo "success 2"
+              - type: script
+                command: for i in $(seq 1 20); do echo "failing build step $i"; done; exit 1
+          name: unimportant-canister
+          build:
+            steps:
+              - type: script
+                command: echo "hide this"
+        "#;
+
+    write_string(
+        &project_dir.join("icp.yaml"), // path
+        &pm,                           // contents
+    )
+    .expect("failed to write project manifest");
+
+    // Invoke build
+    ctx.icp()
+        .current_dir(project_dir)
+        .args(["build"])
+        .assert()
+        .failure()
+        .stdout(contains("success 1"))
+        .stdout(contains("success 2"))
+        .stdout(contains("failing build step 1"))
+        .stdout(contains("failing build step 20"))
+        .stdout(contains("hide this").not());
 }

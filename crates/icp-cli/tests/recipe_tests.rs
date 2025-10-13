20960450b9d01469326773fa935cd37b8c0d676c
ilbertt <luca.bertelli@dfinity.org>
2025-10-13 09:32:27 +0200

    test: PR suggestions

--- crates/icp-cli/tests/recipe_tests.rs
+++ crates/icp-cli/tests/recipe_tests.rs
@@ -1,4 +1,4 @@
-use indoc::formatdoc;
+use indoc::{formatdoc, indoc};
 use k256::sha2::{Digest, Sha256};
 use predicates::{prelude::PredicateBooleanExt, str::contains};
 
@@ -7,6 +7,15 @@ use icp::fs::write_string;
 
 mod common;
 
+const RECIPE_TEMPLATE: &str = indoc! {"
+    build:
+      steps:
+        - type: script
+          command: sh -c 'echo \"test\" > \"$ICP_WASM_OUTPUT_PATH\"'
+"};
+const RECIPE_TEMPLATE_CHECKSUM: &str =
+    "475752eb684a14dcddcb0884c275285b03b53d5519a51925d3e05eb01a494d68";
+
 #[test]
 fn recipe_remote_url_without_checksum() {
     let ctx = TestContext::new();
@@ -14,25 +23,17 @@ fn recipe_remote_url_without_checksum() {
     // Setup project
     let project_dir = ctx.create_project_dir("icp");
 
-    // Create a simple recipe template that generates a pre-built build step
-    let recipe_template = r#"
-build:
-  steps:
-    - type: script
-      command: sh -c 'echo "test" > "$ICP_WASM_OUTPUT_PATH"'
-"#;
-
     // Spawn HTTP server with the recipe template
-    let server = spawn_test_server("GET", "/recipe.hbs", recipe_template.as_bytes());
+    let server = spawn_test_server("GET", "/recipe.hbs", RECIPE_TEMPLATE.as_bytes());
     let addr = server.addr();
 
     // Project manifest
-    let pm = formatdoc! {r#"
+    let pm = formatdoc! {"
         canister:
           name: my-canister
           recipe:
             type: http://{addr}/recipe.hbs
-    "#};
+    "};
 
     write_string(
         &project_dir.join("icp.yaml"), // path
@@ -55,33 +56,25 @@ fn recipe_remote_url_invalid_checksum() {
     // Setup project
     let project_dir = ctx.create_project_dir("icp");
 
-    // Create a simple recipe template
-    let recipe_template = r#"
-build:
-  steps:
-    - type: script
-      command: sh -c 'echo "test" > "$ICP_WASM_OUTPUT_PATH"'
-"#;
-
     // Calculate checksum
     let actual = hex::encode({
         let mut h = Sha256::new();
-        h.update(recipe_template.as_bytes());
+        h.update(RECIPE_TEMPLATE.as_bytes());
         h.finalize()
     });
 
     // Spawn HTTP server with the recipe template
-    let server = spawn_test_server("GET", "/recipe.hbs", recipe_template.as_bytes());
+    let server = spawn_test_server("GET", "/recipe.hbs", RECIPE_TEMPLATE.as_bytes());
     let addr = server.addr();
 
     // Project manifest with invalid checksum
-    let pm = formatdoc! {r#"
+    let pm = formatdoc! {"
         canister:
           name: my-canister
           recipe:
             type: http://{addr}/recipe.hbs
             sha256: invalid
-    "#};
+    "};
 
     write_string(
         &project_dir.join("icp.yaml"), // path
@@ -109,17 +102,9 @@ fn recipe_remote_url_wrong_checksum() {
     // Setup project
     let project_dir = ctx.create_project_dir("icp");
 
-    // Create a simple recipe template
-    let recipe_template = r#"
-build:
-  steps:
-    - type: script
-      command: sh -c 'echo "test" > "$ICP_WASM_OUTPUT_PATH"'
-"#;
-
     let actual = hex::encode({
         let mut h = Sha256::new();
-        h.update(recipe_template.as_bytes());
+        h.update(RECIPE_TEMPLATE.as_bytes());
         h.finalize()
     });
 
@@ -127,17 +112,17 @@ build:
     let expected = "508f107eba1885bd165f4ef75c1b8cf914cf9eda6365f93214bb0cc39bd07ddd";
 
     // Spawn HTTP server with the recipe template
-    let server = spawn_test_server("GET", "/recipe.hbs", recipe_template.as_bytes());
+    let server = spawn_test_server("GET", "/recipe.hbs", RECIPE_TEMPLATE.as_bytes());
     let addr = server.addr();
 
     // Project manifest with wrong checksum
-    let pm = formatdoc! {r#"
+    let pm = formatdoc! {"
         canister:
           name: my-canister
           recipe:
             type: http://{addr}/recipe.hbs
             sha256: {expected}
-    "#};
+    "};
 
     write_string(
         &project_dir.join("icp.yaml"), // path
@@ -165,29 +150,18 @@ fn recipe_remote_url_valid_checksum() {
     // Setup project
     let project_dir = ctx.create_project_dir("icp");
 
-    // Create a simple recipe template
-    let recipe_template = r#"
-build:
-  steps:
-    - type: script
-      command: sh -c 'echo "test" > "$ICP_WASM_OUTPUT_PATH"'
-"#;
-
-    // Hash of the recipe template above
-    let expected_checksum = "f8bd13664a0fd826aea3000b90fc7b4a878854b4f90216924ecfbec4581173cd";
-
     // Spawn HTTP server with the recipe template
-    let server = spawn_test_server("GET", "/recipe.hbs", recipe_template.as_bytes());
+    let server = spawn_test_server("GET", "/recipe.hbs", RECIPE_TEMPLATE.as_bytes());
     let addr = server.addr();
 
     // Project manifest with valid checksum
-    let pm = formatdoc! {r#"
+    let pm = formatdoc! {"
         canister:
           name: my-canister
           recipe:
             type: http://{addr}/recipe.hbs
-            sha256: {expected_checksum}
-    "#};
+            sha256: {RECIPE_TEMPLATE_CHECKSUM}
+    "};
 
     write_string(
         &project_dir.join("icp.yaml"), // path
@@ -210,27 +184,19 @@ fn recipe_local_file_without_checksum() {
     // Setup project
     let project_dir = ctx.create_project_dir("icp");
 
-    // Create a local recipe template file
-    let recipe_template = r#"
-build:
-  steps:
-    - type: script
-      command: sh -c 'echo "test" > "$ICP_WASM_OUTPUT_PATH"'
-"#;
-
     write_string(
         &project_dir.join("recipe.hbs"), // path
-        recipe_template,                 // contents
+        RECIPE_TEMPLATE,                 // contents
     )
     .expect("failed to write recipe template");
 
     // Project manifest without checksum
-    let pm = formatdoc! {r#"
+    let pm = formatdoc! {"
         canister:
           name: my-canister
           recipe:
             type: file://./recipe.hbs
-    "#};
+    "};
 
     write_string(
         &project_dir.join("icp.yaml"), // path
@@ -253,35 +219,20 @@ fn recipe_local_file_invalid_checksum() {
     // Setup project
     let project_dir = ctx.create_project_dir("icp");
 
-    // Create a local recipe template file
-    let recipe_template = r#"
-build:
-  steps:
-    - type: script
-      command: sh -c 'echo "test" > "$ICP_WASM_OUTPUT_PATH"'
-"#;
-
     write_string(
         &project_dir.join("recipe.hbs"), // path
-        recipe_template,                 // contents
+        RECIPE_TEMPLATE,                 // contents
     )
     .expect("failed to write recipe template");
 
-    // Calculate actual checksum
-    let actual = hex::encode({
-        let mut h = Sha256::new();
-        h.update(recipe_template.as_bytes());
-        h.finalize()
-    });
-
     // Project manifest with invalid checksum
-    let pm = formatdoc! {r#"
+    let pm = formatdoc! {"
         canister:
           name: my-canister
           recipe:
             type: file://./recipe.hbs
             sha256: invalid
-    "#};
+    "};
 
     write_string(
         &project_dir.join("icp.yaml"), // path
@@ -298,7 +249,7 @@ build:
         .stderr(
             contains("checksum mismatch")
                 .and(contains("expected invalid"))
-                .and(contains(format!("actual {actual}"))),
+                .and(contains(format!("actual {RECIPE_TEMPLATE_CHECKSUM}"))),
         );
 }
 
@@ -309,34 +260,23 @@ fn recipe_local_file_wrong_checksum() {
     // Setup project
     let project_dir = ctx.create_project_dir("icp");
 
-    // Create a local recipe template file
-    let recipe_template = r#"
-build:
-  steps:
-    - type: script
-      command: sh -c 'echo "test" > "$ICP_WASM_OUTPUT_PATH"'
-"#;
-
     write_string(
         &project_dir.join("recipe.hbs"), // path
-        recipe_template,                 // contents
+        RECIPE_TEMPLATE,                 // contents
     )
     .expect("failed to write recipe template");
-
-    // Hash of the recipe template above
-    let actual = "f8bd13664a0fd826aea3000b90fc7b4a878854b4f90216924ecfbec4581173cd";
 
     // A random wrong but valid checksum
     let expected = "508f107eba1885bd165f4ef75c1b8cf914cf9eda6365f93214bb0cc39bd07ddd";
 
     // Project manifest with wrong checksum
-    let pm = formatdoc! {r#"
+    let pm = formatdoc! {"
         canister:
           name: my-canister
           recipe:
             type: file://./recipe.hbs
             sha256: {expected}
-    "#};
+    "};
 
     write_string(
         &project_dir.join("icp.yaml"), // path
@@ -353,7 +293,7 @@ build:
         .stderr(
             contains("checksum mismatch")
                 .and(contains(format!("expected {expected}")))
-                .and(contains(format!("actual {actual}"))),
+                .and(contains(format!("actual {RECIPE_TEMPLATE_CHECKSUM}"))),
         );
 }
 
@@ -364,31 +304,20 @@ fn recipe_local_file_valid_checksum() {
     // Setup project
     let project_dir = ctx.create_project_dir("icp");
 
-    // Create a local recipe template file
-    let recipe_template = r#"
-build:
-  steps:
-    - type: script
-      command: sh -c 'echo "test" > "$ICP_WASM_OUTPUT_PATH"'
-"#;
-
     write_string(
         &project_dir.join("recipe.hbs"), // path
-        recipe_template,                 // contents
+        RECIPE_TEMPLATE,                 // contents
     )
     .expect("failed to write recipe template");
 
-    // Hash of the recipe template above
-    let expected_checksum = "f8bd13664a0fd826aea3000b90fc7b4a878854b4f90216924ecfbec4581173cd";
-
     // Project manifest with valid checksum
-    let pm = formatdoc! {r#"
+    let pm = formatdoc! {"
         canister:
           name: my-canister
           recipe:
             type: file://./recipe.hbs
-            sha256: {expected_checksum}
-    "#};
+            sha256: {RECIPE_TEMPLATE_CHECKSUM}
+    "};
 
     write_string(
         &project_dir.join("icp.yaml"), // path
@@ -412,13 +341,13 @@ fn recipe_builtin_ignores_checksum() {
         // Setup project
         let project_dir = ctx.create_project_dir("icp");
 
-        let pm = formatdoc! {r#"
+        let pm = formatdoc! {"
             canister:
             name: my-canister
             recipe:
                 type: {recipe_type}
                 sha256: invalid_checksum_should_be_ignored
-        "#};
+        "};
 
         write_string(
             &project_dir.join("icp.yaml"), // path

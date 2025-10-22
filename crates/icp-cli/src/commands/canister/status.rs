bab3d2f40243b125d82f9a68d83f979ebcda1a01
Raymond Khalife <raymond.khalife@dfinity.org>
2025-10-22 18:42:58 -0400

    conver canister status

--- crates/icp-cli/src/commands/canister/status.rs
+++ crates/icp-cli/src/commands/canister/status.rs
@@ -1,95 +1,29 @@
 use clap::Args;
 use ic_agent::{AgentError, export::Principal};
 use ic_management_canister_types::{CanisterStatusResult, LogVisibility};
-use icp::{agent, identity, network};
 
-use crate::{
-    commands::Context,
-    options::{EnvironmentOpt, IdentityOpt},
-    store_id::{Key, LookupError as LookupIdError},
+use crate::commands::{
+    Context,
+    args::{self, ArgValidationError},
 };
 
 #[derive(Debug, Args)]
 pub(crate) struct StatusArgs {
-    /// The name of the canister within the current project
-    pub(crate) name: String,
-
     #[command(flatten)]
-    identity: IdentityOpt,
-
-    #[command(flatten)]
-    environment: EnvironmentOpt,
+    pub(crate) cmd_args: args::CanisterCommandArgs,
 }
 
 #[derive(Debug, thiserror::Error)]
 pub(crate) enum CommandError {
     #[error(transparent)]
-    Project(#[from] icp::LoadError),
-
-    #[error(transparent)]
-    Identity(#[from] identity::LoadError),
-
-    #[error("project does not contain an environment named '{name}'")]
-    EnvironmentNotFound { name: String },
-
-    #[error(transparent)]
-    Access(#[from] network::AccessError),
-
-    #[error(transparent)]
-    Agent(#[from] agent::CreateError),
-
-    #[error("environment '{environment}' does not include canister '{canister}'")]
-    EnvironmentCanister {
-        environment: String,
-        canister: String,
-    },
-
-    #[error(transparent)]
-    Lookup(#[from] LookupIdError),
-
-    #[error(transparent)]
     Status(#[from] AgentError),
+
+    #[error(transparent)]
+    Shared(#[from] ArgValidationError),
 }
 
 pub(crate) async fn exec(ctx: &Context, args: &StatusArgs) -> Result<(), CommandError> {
-    // Load project
-    let p = ctx.project.load().await?;
-
-    // Load identity
-    let id = ctx.identity.load(args.identity.clone().into()).await?;
-
-    // Load target environment
-    let env =
-        p.environments
-            .get(args.environment.name())
-            .ok_or(CommandError::EnvironmentNotFound {
-                name: args.environment.name().to_owned(),
-            })?;
-
-    // Access network
-    let access = ctx.network.access(&env.network).await?;
-
-    // Agent
-    let agent = ctx.agent.create(id, &access.url).await?;
-
-    if let Some(k) = access.root_key {
-        agent.set_root_key(k);
-    }
-
-    // Ensure canister is included in the environment
-    if !env.canisters.contains_key(&args.name) {
-        return Err(CommandError::EnvironmentCanister {
-            environment: env.name.to_owned(),
-            canister: args.name.to_owned(),
-        });
-    }
-
-    // Lookup the canister id
-    let cid = ctx.ids.lookup(&Key {
-        network: env.network.name.to_owned(),
-        environment: env.name.to_owned(),
-        canister: args.name.to_owned(),
-    })?;
+    let (cid, agent) = args.cmd_args.get_cid_and_agent(ctx).await?;
 
     // Management Interface
     let mgmt = ic_utils::interfaces::ManagementCanister::create(&agent);

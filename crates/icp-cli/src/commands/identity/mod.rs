93619ee3d490a277648cb4e7788ced6f0eafeff0
Raymond Khalife <raymond.khalife@dfinity.org>
2025-11-19 14:43:54 -0500

    docs: Add missing docs to cli commands

--- crates/icp-cli/src/commands/identity/mod.rs
+++ crates/icp-cli/src/commands/identity/mod.rs
@@ -8,9 +8,18 @@ pub(crate) mod principal;
 
 #[derive(Debug, Subcommand)]
 pub(crate) enum Command {
+    /// Display the currently selected identity
     Default(default::DefaultArgs),
+
+    /// Import a new identity
     Import(import::ImportArgs),
+
+    /// List the identities
     List(list::ListArgs),
+
+    /// Create a new identity
     New(new::NewArgs),
+
+    /// Display the principal for the current identity
     Principal(principal::PrincipalArgs),
 }

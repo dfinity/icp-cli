deeea048029ede8b5fdbf5bb5975574783ba9b76
Vivienne <vivienne.siffert@dfinity.org>
2026-01-15 11:09:01 +0100

    fix tests

---
+++ examples/icp-recipe-remote-url/test.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -e
+
+# Start the HTTP server in the background
+python3 -m http.server 8080 &
+SERVER_PID=$!
+
+# Give the server a moment to start
+sleep 1
+
+# Function to cleanup server on exit
+cleanup() {
+    echo "Stopping HTTP server (PID: $SERVER_PID)"
+    kill $SERVER_PID 2>/dev/null
+    wait $SERVER_PID 2>/dev/null
+}
+
+# Ensure cleanup happens on script exit
+trap cleanup EXIT
+
+# Run icp project show
+icp project show
+
+exit $?

45f09269555b958dedb65cdd47aef4299a601703
Adam Spofford <adam.spofford@dfinity.org>
2026-01-29 08:14:36 -0800

    .

--- .github/scripts/provision-windows-test.ps1
+++ .github/scripts/provision-windows-test.ps1
@@ -3,4 +3,4 @@ $ErrorActionPreference = "Stop"
 $PSNativeCommandUseErrorActionPreference = $true
 
 Invoke-WebRequest -Uri 'https://github.com/disig/SoftHSM2-for-Windows/releases/download/v2.5.0/SoftHSM2-2.5.0-portable.zip' -OutFile 'softhsm2.zip'
-Expand-Archive -Path 'softhsm2.zip' -DestinationPath 'C:\SoftHSM2'
+Expand-Archive -Path 'softhsm2.zip' -DestinationPath 'C:\'

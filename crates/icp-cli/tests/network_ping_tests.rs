4ec4acf0c8cb12d2307b5327dfba026c1718a1f4
Adam Spofford <adam.spofford@dfinity.org>
2026-05-22 12:47:31 -0700

    test

--- crates/icp-cli/tests/network_ping_tests.rs
+++ crates/icp-cli/tests/network_ping_tests.rs
@@ -85,3 +85,61 @@ fn ping_not_in_project() {
         .failure()
         .stderr(contains("Error: failed to locate project directory").trim());
 }
+
+#[tokio::test]
+async fn ping_url() {
+    let ctx = TestContext::new();
+
+    let project_dir = ctx.create_project_dir("icp");
+
+    // Project manifest
+    write_string(
+        &project_dir.join("icp.yaml"), // path
+        NETWORK_RANDOM_PORT,           // contents
+    )
+    .expect("failed to write project manifest");
+
+    let _child_guard = ctx.start_network_in(&project_dir, "random-network").await;
+
+    let network_descriptor = ctx.wait_for_network_descriptor(&project_dir, "random-network");
+    let expected_root_key = network_descriptor
+        .root_key
+        .into_iter()
+        .map(|byte| Value::Number(serde_json::Number::from(byte)))
+        .collect::<Vec<Value>>();
+
+    let output = ctx
+        .icp()
+        .current_dir(&project_dir)
+        .args([
+            "network",
+            "ping",
+            &format!("http://localhost:{}", network_descriptor.gateway_port),
+        ])
+        .assert()
+        .success()
+        .get_output()
+        .stdout
+        .clone();
+
+    let json: Value = serde_json::from_slice(&output).expect("stdout was not valid JSON");
+
+    let root_key = json
+        .get("root_key")
+        .expect("missing 'root_key' field")
+        .as_array()
+        .expect("'root_key' was not an array");
+
+    assert_eq!(
+        root_key, &expected_root_key,
+        "unexpected value for 'root_key'"
+    );
+
+    let status = json
+        .get("replica_health_status")
+        .expect("missing 'replica_health_status' field")
+        .as_str()
+        .expect("'replica_health_status' was not a string");
+
+    assert_eq!(status, "healthy", "unexpected replica_health_status");
+}
